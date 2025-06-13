47f2958930b979ff9a2144f1cd7468483699d1df
João Jales <joaoricardocirino@gmail.com>
2025-06-13 05:14:37 -0300

    fix: corrige erro de declaração

--- include/multiplayer.h
+++ include/multiplayer.h
@@ -10,11 +10,11 @@
 void multiplayer();
 void cadastrar_jogadores(JogadorInfo* jogadores);
 void apostas(JogadorInfo* jogadores, double* valoresApostas);
-
 void distribuicao_cartas_iniciais_dealer_multiplayer(DealerInfo* dealer);
 void distribuicao_cartas_iniciais_jogadores_multiplayer(JogadorInfo* jogadores);
 void tela_mesa(JogadorInfo* jogadores, DealerInfo* dealer, int numJogadores, int esconderCartaDealer, double* valoresApostas, int numRodadas, int rodadaAtual);
 void resultados_rodada(int pontos_dealer, JogadorInfo* jogadores, double* valoresApostas, int *numJogadoresAtivos);
+void resultado_partida(JogadorInfo* jogagores);
 
 
 #endif
